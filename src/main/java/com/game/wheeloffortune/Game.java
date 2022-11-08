f7c5451e72d648d25585813293ddcc99ad9e9666
the-mighty-fall <vvarfare@gmail.com>
2022-11-08 17:30:25 -0600

    More changes

--- src/main/java/com/game/wheeloffortune/Game.java
+++ src/main/java/com/game/wheeloffortune/Game.java
@@ -1,5 +1,32 @@
 package com.game.wheeloffortune;
 
+import java.util.List;
+
 public class Game {
+
+    private static List<Player> players;
+    private static String currentRoundPuzzle;
+    private static String currentRoundCategory;
+
+    private static int currentRoundNumber;
+
+    private static List<String> consonants;
+    private static List<String> vowels;
+
+
+    // constructors
+    public Game() {
+    }
+
+
+    // business
+
+    // accessors
+
+
+    @Override
+    public String toString() {
+        return "Game{}";
+    }
 
 }
