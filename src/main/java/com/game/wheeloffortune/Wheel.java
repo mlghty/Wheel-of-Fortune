5c9fa368a49c470ef791cf28bd30cdcbf4dbdc38
Joe Racke <joeracke@gmail.com>
2022-11-08 15:41:19 -0800

    Wheel.java basic overview

--- src/main/java/com/game/wheeloffortune/Wheel.java
+++ src/main/java/com/game/wheeloffortune/Wheel.java
@@ -1,4 +1,10 @@
 package com.game.wheeloffortune;
 
 public class Wheel {
+    private final int[] WEDGE_VALUES_OF_WHEEL = {250,300,350}; // 24 Total I'm thinking a -1 will mean Bankrupt, 0 can be losing a turn
+
+    // returns a random wedge on the wheel
+    public static int getRandomWedgeOfTheWheel() {
+        return 0;
+    }
 }
