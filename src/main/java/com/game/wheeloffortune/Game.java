287a08e5ec923d0e6bdc78a019ae3d34b5a6459c
the-mighty-fall <vvarfare@gmail.com>
2022-11-08 17:55:59 -0600

    Added accessors for Game.class

--- src/main/java/com/game/wheeloffortune/Game.java
+++ src/main/java/com/game/wheeloffortune/Game.java
@@ -24,6 +24,54 @@ public class Game {
     // accessors
 
 
+    public static List<Player> getPlayers() {
+        return players;
+    }
+
+    public static void setPlayers(List<Player> players) {
+        Game.players = players;
+    }
+
+    public static String getCurrentRoundPuzzle() {
+        return currentRoundPuzzle;
+    }
+
+    public static void setCurrentRoundPuzzle(String currentRoundPuzzle) {
+        Game.currentRoundPuzzle = currentRoundPuzzle;
+    }
+
+    public static String getCurrentRoundCategory() {
+        return currentRoundCategory;
+    }
+
+    public static void setCurrentRoundCategory(String currentRoundCategory) {
+        Game.currentRoundCategory = currentRoundCategory;
+    }
+
+    public static int getCurrentRoundNumber() {
+        return currentRoundNumber;
+    }
+
+    public static void setCurrentRoundNumber(int currentRoundNumber) {
+        Game.currentRoundNumber = currentRoundNumber;
+    }
+
+    public static List<String> getConsonants() {
+        return consonants;
+    }
+
+    public static void setConsonants(List<String> consonants) {
+        Game.consonants = consonants;
+    }
+
+    public static List<String> getVowels() {
+        return vowels;
+    }
+
+    public static void setVowels(List<String> vowels) {
+        Game.vowels = vowels;
+    }
+
     @Override
     public String toString() {
         return "Game{}";
