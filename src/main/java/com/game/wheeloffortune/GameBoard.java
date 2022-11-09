bd84fe0b37a7a8d082ec89ad6b36cac666593ef0
Joe Racke <joeracke@gmail.com>
2022-11-09 15:36:38 -0800

    added the basic GameBoard class and the test for it

---
+++ src/main/java/com/game/wheeloffortune/GameBoard.java
@@ -0,0 +1,81 @@
+package com.game.wheeloffortune;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GameBoard {
+    private String gamePuzzle;
+    private boolean[] isLetterRevealed;
+    private String gameHint;
+    List<Character> lettersAlreadyGuessed = new ArrayList<>();
+
+    // Constructor
+    public GameBoard(String gamePuzzle, String gameHint) {
+        setGamePuzzle(gamePuzzle.toUpperCase());
+        setIsLetterRevealed(new boolean[gamePuzzle.length()]);
+        setGameHint(gameHint.toUpperCase());
+    }
+
+    // Business Methods
+    public int guessLetter(char letterGuessed) {
+        int numberOfTimesLetterAppears = 0;
+        letterGuessed = Character.toUpperCase(letterGuessed);
+        if(lettersAlreadyGuessed.contains(letterGuessed)) {
+            System.out.println("That letter was already guessed.");
+            return 0;
+        } else {
+            lettersAlreadyGuessed.add(letterGuessed);
+        }
+        for(int i = 0; i < gamePuzzle.length(); i++) {
+            if(gamePuzzle.charAt(i) == letterGuessed) {
+                isLetterRevealed[i] = true;
+                numberOfTimesLetterAppears++;
+            }
+        }
+        return numberOfTimesLetterAppears;
+    }
+
+    public boolean solvePuzzle(String puzzleGuess) {
+        if(puzzleGuess.toUpperCase().equals(this.gamePuzzle)) {
+            System.out.println(puzzleGuess + " was correct!");
+            return true;
+        }
+        System.out.println(puzzleGuess + " was incorrect.");
+        return false;
+    }
+
+    // Accessors
+    public String getGamePuzzle() {
+        String returnValue = "";
+        for (int i = 0; i < this.gamePuzzle.length(); i++) {
+            if (gamePuzzle.charAt(i) == ' ') {
+                returnValue = returnValue.concat(" ");
+            } else if (isLetterRevealed[i]) {
+                returnValue = returnValue.concat(String.valueOf(gamePuzzle.charAt(i)));
+            } else {
+                returnValue = returnValue.concat("\u25A0");
+            }
+        }
+        return returnValue;
+    }
+
+    public void setGamePuzzle(String gamePuzzle) {
+        this.gamePuzzle = gamePuzzle;
+    }
+
+    public boolean[] getIsLetterRevealed() {
+        return isLetterRevealed;
+    }
+
+    public void setIsLetterRevealed(boolean[] isLetterRevealed) {
+        this.isLetterRevealed = isLetterRevealed;
+    }
+
+    public String getGameHint() {
+        return gameHint;
+    }
+
+    public void setGameHint(String gameHint) {
+        this.gameHint = gameHint;
+    }
+}
---
+++ src/test/java/com/game/wheeloffortune/GameBoardTest.java
@@ -0,0 +1,35 @@
+package com.game.wheeloffortune;
+
+import java.util.Scanner;
+
+public class GameBoardTest {
+    public static void main(String[] args) {
+        String gamePuzzle = "Sang is the best Java Instructor";
+        String gameHint = "General Java Knowledge";
+        boolean puzzleSolved = false;
+        GameBoard gameBoard = new GameBoard(gamePuzzle, gameHint);
+
+        while(!puzzleSolved) {
+            int choice;
+            Scanner option = new Scanner(System.in);
+            System.out.println(gameBoard.getGamePuzzle());
+            System.out.println("1. Guess Letter 2. Solve Puzzle");
+            choice = option.nextInt();
+            option.nextLine();
+            switch(choice) {
+                case 1:
+                    System.out.println("Choose a letter");
+                    String letter = option.next();
+                    option.nextLine();
+                    gameBoard.guessLetter(letter.charAt(0));
+                    break;
+                case 2:
+                    System.out.println("Attempt to solve the puzzle: ");
+                    String solution = option.nextLine().strip();
+                    puzzleSolved = gameBoard.solvePuzzle(solution);
+                    break;
+            }
+        }
+
+    }
+}
