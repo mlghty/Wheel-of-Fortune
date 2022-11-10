9be4c2a347a014a9b43c286edd05c1366fb20696
Joe Racke <joeracke@gmail.com>
2022-11-10 13:35:58 -0800

    Game Class updates, making it non-static, adding GameBoard fields and constructors

--- src/main/java/com/game/wheeloffortune/Puzzles.java
+++ src/main/java/com/game/wheeloffortune/Puzzles.java
@@ -4,10 +4,12 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Puzzles {
-    Map<String, String> hintPuzzles = new HashMap<String, String>(){{
+    Map<String, String> hintPuzzles = new HashMap<>() {{
         hintPuzzles.put("Objects In Memory", "REFERENCE VARIABLE");
         hintPuzzles.put("Data Type", "PRIMITIVE");
         hintPuzzles.put("Food", "HOT DOGS ARE NOT TACOS");
         hintPuzzles.put("Something Thrown", "EXCEPTIONS");
     }};
+
+    // TODO method public static Map.Entry<K,V> getRandomPuzzle()
 }
--- src/test/java/com/game/wheeloffortune/GameTest.java
+++ src/test/java/com/game/wheeloffortune/GameTest.java
@@ -2,13 +2,26 @@ package com.game.wheeloffortune;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
 
 import static org.junit.Assert.*;
 
 public class GameTest {
 
+    private Game newGame;
+    private Player player1 = new Player();
+    private Player player2 = new Player();
     @Before
     public void setUp() throws Exception {
+        newGame = new Game(player1);
+    }
+
+    @Test
+    public void testNewRound() {
+        newGame.startRound();
+        System.out.println(newGame.getCurrentGameBoard());
     }
 
     @After
