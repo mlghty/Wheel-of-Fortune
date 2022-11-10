0d9b2954ce2e10cdd9a60a88e53802d4dae25626
the-mighty-fall <vvarfare@gmail.com>
2022-11-10 17:12:36 -0600

    Initial GameDialogue commit

--- src/main/java/com/game/wheeloffortune/GameDialogue.java
+++ src/main/java/com/game/wheeloffortune/GameDialogue.java
@@ -9,12 +9,18 @@ public class GameDialogue {
 
     private Integer numOfPlayer;
     private Scanner myObj;
-    private static List<Player> players;
+    private List<Player> players;
+    private Game currentGame;
 
 
     public GameDialogue() {
         players = new ArrayList<>();
         myObj = new Scanner(System.in);
+//        currentGame = new Game();
+        currentGame = new Game( new Player("jhoe"));
+        currentGame.startRound();
+
+
         numOfPlayer = 1;
     }
 
@@ -38,7 +44,7 @@ public class GameDialogue {
                 System.out.println("Exiting game...");
                 break;
             } else {
-                System.out.println("Error valid options are 'X' & 'Q'" );
+                System.out.println("Error valid options are 'X' & 'Q'");
                 System.out.println("Press 'X' to Start Game: ");
                 System.out.println("Press 'Q' to Exit Game: ");
                 pressedKey = myObj.nextLine();  // Read user input
@@ -51,15 +57,14 @@ public class GameDialogue {
     }
 
 
-
-    public int numberOfPlayers(){
+    public int numberOfPlayers() {
 
         int numPlayers = 0;
         System.out.println("Select number of players [1-3]");
 
         Integer playerCount = Integer.parseInt(myObj.nextLine());
 
-        while (playerCount < 0 || playerCount > 3){
+        while (playerCount < 0 || playerCount > 3) {
             System.out.println("Error please select between 1-3 players...");
             playerCount = Integer.parseInt(myObj.nextLine());
 
@@ -72,18 +77,50 @@ public class GameDialogue {
     }
 
 
+    public void setPlayerNames() {
 
-    public void setPlayerNames(){
-
-        for (int i = 1; i <= numOfPlayer; i++){
+        for (int i = 1; i <= numOfPlayer; i++) {
             System.out.println("Please enter name for Player " + i);
             String playerName = myObj.nextLine();
             players.add(new Player(playerName));
         }
 
+        // sets players in game class after creating & assigning names
+        currentGame.setPlayers(players);
+
         System.out.println(players);
 
     }
+
+
+    public void displayCurrentPuzzle() {
+//        currentGame.startRound();
+        GameBoard currentGameBoard = currentGame.getCurrentGameBoard();
+        String puzzleHint = currentGameBoard.getGameHint();
+        System.out.println("Hint: " + puzzleHint);
+        String puzzle = currentGameBoard.getGamePuzzle();
+        System.out.println("Puzzle: " + puzzle);
+        System.out.println("\n \n \n \n \n \n");
+    }
+
+
+    public void gameLoop(){
+
+        System.out.println("Spinning Wheel...");
+        System.out.println("Wheel Value:" + currentGame.getCurrentPlayersTurn().spinWheel());
+
+        System.out.println("Options...");
+        System.out.println("Press 1 to guess a letter.");
+        System.out.println("Press 2 to buy a vowel.");
+        System.out.println("Press 3 to do something...");
+        String pressedKey = myObj.nextLine();
+
+    }
+
+
+
+
+
 
 }
 
@@ -94,7 +131,11 @@ class Main1 {
 
 //        gameDialogue.startGame();
 //        gameDialogue.numberOfPlayers();
-        gameDialogue.setPlayerNames();
+//        gameDialogue.setPlayerNames();
+//        gameDialogue.displayCurrentPuzzle();
+
+        gameDialogue.gameLoop();
+
 
 
     }
