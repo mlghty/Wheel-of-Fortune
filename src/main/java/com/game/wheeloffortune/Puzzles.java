ae41c19707603964f3719ae71b3078eb6cc087ba
Cynthia Pottin <cindy.pottin@gmail.com>
2022-11-10 16:00:26 -0500

    added puzzle class

---
+++ src/main/java/com/game/wheeloffortune/Puzzles.java
@@ -0,0 +1,13 @@
+package com.game.wheeloffortune;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Puzzles {
+    Map<String, String> hintPuzzles = new HashMap<String, String>(){{
+        hintPuzzles.put("Objects In Memory", "REFERENCE VARIABLE");
+        hintPuzzles.put("Data Type", "PRIMITIVE");
+        hintPuzzles.put("Food", "HOT DOGS ARE NOT TACOS");
+        hintPuzzles.put("Something Thrown", "EXCEPTIONS");
+    }};
+}
