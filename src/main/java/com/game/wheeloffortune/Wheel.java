3b8e0e15f86cca757b6efbec946948d0ecc39f2f
Cynthia Pottin <cindy.pottin@gmail.com>
2022-11-10 09:36:20 -0500

    updated wheel class with random and wedge values

--- src/main/java/com/game/wheeloffortune/Wheel.java
+++ src/main/java/com/game/wheeloffortune/Wheel.java
@@ -1,10 +1,17 @@
 package com.game.wheeloffortune;
 
+import java.util.Random;
+
 public class Wheel {
-    private final int[] WEDGE_VALUES_OF_WHEEL = {250,300,350}; // 24 Total I'm thinking a -1 will mean Bankrupt, 0 can be losing a turn
+    // 24 Total I'm thinking a -1 will mean Bankrupt, 0 can be losing a turn
+
+    private final int[] WEDGE_VALUES_OF_WHEEL = {200,250,300,350,400,450,500,550,600,
+            650,700,750,800,850,900,950,1000,1050,1100,1150,1200,1250,0,-1};
 
     // returns a random wedge on the wheel
-    public static int getRandomWedgeOfTheWheel() {
-        return 0;
+    public static int getRandomWedgeOfTheWheel(int [] WEDGE_VALUES_OF_WHEEL) {
+        int randomPrizeValue = new Random().nextInt(WEDGE_VALUES_OF_WHEEL.length);
+        return WEDGE_VALUES_OF_WHEEL[randomPrizeValue];
     }
+
 }
