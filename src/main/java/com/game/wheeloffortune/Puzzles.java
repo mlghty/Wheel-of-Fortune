770dc4e802872d5a740d9d2fd3261e7a6ea32472
Cynthia Pottin <cindy.pottin@gmail.com>
2022-11-11 07:29:10 -0500

    add to Puzzle, get random hint/puzzle for game(review with team)

--- src/main/java/com/game/wheeloffortune/Puzzles.java
+++ src/main/java/com/game/wheeloffortune/Puzzles.java
@@ -1,15 +1,30 @@
 package com.game.wheeloffortune;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Puzzles {
-    Map<String, String> hintPuzzles = new HashMap<>() {{
+
+    Map<String, String> hintPuzzles = new HashMap<>();
+
+    {
         hintPuzzles.put("Objects In Memory", "REFERENCE VARIABLE");
         hintPuzzles.put("Data Type", "PRIMITIVE");
         hintPuzzles.put("Food", "HOT DOGS ARE NOT TACOS");
         hintPuzzles.put("Something Thrown", "EXCEPTIONS");
-    }};
 
-    // TODO method public static Map.Entry<K,V> getRandomPuzzle()
+
+        // TODO method public static Map.Entry<K,V> getRandomPuzzle()
+        //Talk to the team about this
+    Random randomKVPairIndex = new Random();
+    int upperbound = hintPuzzles.size();
+    int randomNumber = randomKVPairIndex.nextInt(upperbound);
+
+    List keys = new ArrayList(hintPuzzles.keySet());
+    List values = new ArrayList(hintPuzzles.values());
+    String gameHint = (String)keys.get(randomNumber);
+    String gamePuzzle = (String) values.get(randomNumber);
+
+
+    }
 }
+
--- src/test/java/com/game/wheeloffortune/WheelTest.java
+++ src/test/java/com/game/wheeloffortune/WheelTest.java
@@ -2,6 +2,7 @@ package com.game.wheeloffortune;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
@@ -10,6 +11,8 @@ public class WheelTest {
     @Before
     public void setUp() throws Exception {
     }
+
+
 
     @After
     public void tearDown() throws Exception {
