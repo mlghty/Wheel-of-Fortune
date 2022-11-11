41fee771d969d5e4edddf7a30df65be0a5fe866f
Cynthia Pottin <cindy.pottin@gmail.com>
2022-11-11 15:08:42 -0500

    Deprecated Methods in Player and trying to get Map.Entry return random to work in Puzzles Class

--- src/main/java/com/game/wheeloffortune/Player.java
+++ src/main/java/com/game/wheeloffortune/Player.java
@@ -14,32 +14,12 @@ public class Player {
 
     // business
 
-    //
-
-    public int buyVowel(char vowelBOUGHT) {
-//do some sort of validation that the letter is in the VOWELS
-        vowelBOUGHT = Character.toUpperCase(vowelBOUGHT);
-//        if(VOWELS.contains(vowelBOUGHT))
-        currentRoundMoney = (currentRoundMoney - 250);
-        return currentRoundMoney;
-    }
-
     public int spinWheel() {
         int wheelValue = Wheel.getRandomWedgeOfTheWheel();
         return wheelValue;
     }
 
-    public void pickConsonants() {
-        //do some validation of if the letter is in the CONSONANTS list
-//    int numberOfTimesLetterAppears = GameBoard.guessLetter;
-//        currentRoundMoney = (currentRoundMoney +
-//                (numberOfTimesLetterAppears * wheelValue));
 
-    }
-
-    public boolean solvePuzzle(String Puzzle) {
-        return false;
-    }
 
     // accessors
     public String getName() {
@@ -75,3 +55,34 @@ public class Player {
     }
 
 }
+
+// Deprecated Code - Handled in GameBoard
+
+//    public int buyVowel(char vowelBOUGHT) {
+////do some sort of validation that the letter is in the VOWELS
+//        vowelBOUGHT = Character.toUpperCase(vowelBOUGHT);
+////        if(VOWELS.contains(vowelBOUGHT))
+//        currentRoundMoney = (currentRoundMoney - 250);
+//        return currentRoundMoney;
+//    }
+
+//    public boolean solvePuzzle(String puzzle) {
+////        while(true)
+////        if(myobj.nextline().equals(puzzle)){
+//        totalMoney = totalMoney + currentRoundMoney;
+////            System.out.println("You Win!");
+////        }
+////        else{
+//
+////            System.out.println("Sorry you've guessed wrong");
+////        }
+//        return false;
+//        }
+
+//    public void pickConsonants() {
+//        //do some validation of if the letter is in the CONSONANTS list
+////    int numberOfTimesLetterAppears = GameBoard.guessLetter;
+////        currentRoundMoney = (currentRoundMoney +
+////                (numberOfTimesLetterAppears * wheelValue));
+//
+//    }
--- src/main/java/com/game/wheeloffortune/Puzzles.java
+++ src/main/java/com/game/wheeloffortune/Puzzles.java
@@ -1,30 +1,46 @@
 package com.game.wheeloffortune;
 
+import org.junit.jupiter.api.Test;
+import org.sonatype.guice.plexus.config.Hints;
+
 import java.util.*;
 
 public class Puzzles {
 
-    Map<String, String> hintPuzzles = new HashMap<>();
+    private static Map<String, String> hintPuzzles = new HashMap<String,String>(){
+        {
 
-    {
-        hintPuzzles.put("Objects In Memory", "REFERENCE VARIABLE");
-        hintPuzzles.put("Data Type", "PRIMITIVE");
-        hintPuzzles.put("Food", "HOT DOGS ARE NOT TACOS");
-        hintPuzzles.put("Something Thrown", "EXCEPTIONS");
+            hintPuzzles.put("REFERENCE VARIABLE", "Objects In Memory");
+            hintPuzzles.put("PRIMITIVE", "Data Type");
+            hintPuzzles.put("HOT DOGS ARE NOT TACOS", "Food");
+            hintPuzzles.put("EXCEPTIONS", "Something Thrown");
+        }};
 
 
         // TODO method public static Map.Entry<K,V> getRandomPuzzle()
-        //Talk to the team about this
-    Random randomKVPairIndex = new Random();
-    int upperbound = hintPuzzles.size();
-    int randomNumber = randomKVPairIndex.nextInt(upperbound);
+        public static Map.Entry<String,String> getRandomPuzzle() {
+            Map.Entry<String, String> hintpuzz = Map.entry("", "");
+            for (Map.Entry<String, String> hp : hintPuzzles.entrySet()) {
+                hintpuzz = hp;
+                break;
+            }
+            return hintpuzz;
 
-    List keys = new ArrayList(hintPuzzles.keySet());
-    List values = new ArrayList(hintPuzzles.values());
-    String gameHint = (String)keys.get(randomNumber);
-    String gamePuzzle = (String) values.get(randomNumber);
+//        Random randomKVPairIndex = new Random();
+//        int upperbound = hintPuzzles.size();
+//        int randomNumber = randomKVPairIndex.nextInt(upperbound);
+//
+//        List keys = new ArrayList(hintPuzzles.keySet());
+//        List values = new ArrayList(hintPuzzles.values());
+//        String gameHint = (String) keys.get(randomNumber);
+//        String gamePuzzle = (String) values.get(randomNumber);
 
 
+        }
+
+    public static void main(String[] args) {
+        System.out.println(hintPuzzles.get("REFERENCE VARIABLE"));
     }
 }
+
 
