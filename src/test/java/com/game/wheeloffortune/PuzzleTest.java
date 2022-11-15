291e9d034e035eeea0d6832e37a0e78cfde1ebd7
Cynthia Pottin <cindy.pottin@gmail.com>
2022-11-15 11:23:54 -0500

    Testing Wheel, added PuzzleTest class

---
+++ src/test/java/com/game/wheeloffortune/PuzzleTest.java
@@ -0,0 +1,4 @@
+package com.game.wheeloffortune;
+
+public class PuzzleTest {
+}
--- src/test/java/com/game/wheeloffortune/WheelTest.java
+++ src/test/java/com/game/wheeloffortune/WheelTest.java
@@ -1,18 +1,23 @@
 package com.game.wheeloffortune;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class WheelTest {
-
     @Before
     public void setUp() throws Exception {
+
     }
-
-
+@Test
+public void setTestingWheelMethod() {
+    Wheel testingWheelMethod = new Wheel();
+    Assert.assertNotNull(testingWheelMethod.
+            getRandomWedgeOfTheWheel());
+}
 
     @After
     public void tearDown() throws Exception {
