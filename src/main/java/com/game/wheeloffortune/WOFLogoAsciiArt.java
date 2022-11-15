46888312742aea95ec94da6a224f8d2218570bd3
Cynthia Pottin <cindy.pottin@gmail.com>
2022-11-15 13:38:44 -0500

    adjust WOF placement

--- src/main/java/com/game/wheeloffortune/WOFLogoAsciiArt.java
+++ src/main/java/com/game/wheeloffortune/WOFLogoAsciiArt.java
@@ -7,9 +7,9 @@ public class WOFLogoAsciiArt {
 
     public static void printWOFLogo() {
         //x axis
-        int width = 200;
+        int width = 170;
         //y axis
-        int height = 20;
+        int height = 15;
 
 
         //constructor new buffered image
@@ -23,7 +23,7 @@ public class WOFLogoAsciiArt {
 
         Graphics2D WOFGraphic = (Graphics2D) WOF;
         WOFGraphic.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
-        WOF.drawString("WHEEL OF FORTUNE", 5, 15);
+        WOF.drawString("WHEEL OF FORTUNE", 5, 13);
 
         for (int y = 0; y < height; y++) {
             StringBuilder sb = new StringBuilder();
