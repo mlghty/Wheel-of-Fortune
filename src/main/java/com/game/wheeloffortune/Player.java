5318da86a8603b85e64529b882ce71b853192484
Joe Racke <joeracke@gmail.com>
2022-11-15 12:05:10 -0800

    Added tests to the GameTest.java

--- src/main/java/com/game/wheeloffortune/Player.java
+++ src/main/java/com/game/wheeloffortune/Player.java
@@ -15,8 +15,7 @@ public class Player {
     // business
 
     public int spinWheel() {
-        int wheelValue = Wheel.getRandomWedgeOfTheWheel();
-        return wheelValue;
+        return Wheel.getRandomWedgeOfTheWheel();
     }
 
     // accessors
@@ -53,34 +52,3 @@ public class Player {
     }
 
 }
-
-// Deprecated Code - Handled in GameBoard
-
-//    public int buyVowel(char vowelBOUGHT) {
-////do some sort of validation that the letter is in the VOWELS
-//        vowelBOUGHT = Character.toUpperCase(vowelBOUGHT);
-////        if(VOWELS.contains(vowelBOUGHT))
-//        currentRoundMoney = (currentRoundMoney - 250);
-//        return currentRoundMoney;
-//    }
-
-//    public boolean solvePuzzle(String puzzle) {
-////        while(true)
-////        if(myobj.nextline().equals(puzzle)){
-//        totalMoney = totalMoney + currentRoundMoney;
-////            System.out.println("You Win!");
-////        }
-////        else{
-//
-////            System.out.println("Sorry you've guessed wrong");
-////        }
-//        return false;
-//        }
-
-//    public void pickConsonants() {
-//        //do some validation of if the letter is in the CONSONANTS list
-////    int numberOfTimesLetterAppears = GameBoard.guessLetter;
-////        currentRoundMoney = (currentRoundMoney +
-////                (numberOfTimesLetterAppears * wheelValue));
-//
-//    }
--- src/test/java/com/game/wheeloffortune/GameTest.java
+++ src/test/java/com/game/wheeloffortune/GameTest.java
@@ -4,16 +4,14 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.List;
-import java.util.Scanner;
-
 import static org.junit.Assert.*;
 
 public class GameTest {
 
-    private Game newGame,newGame2;
+    private Game newGame, newGame2;
     private Player player1 = new Player("Joe");
     private Player player2 = new Player("Cindy");
+
     @Before
     public void setUp() throws Exception {
         newGame = new Game(player1);
@@ -38,33 +36,8 @@ public class GameTest {
     public void tearDown() throws Exception {
     }
 
-    // Test for the Try-Catch of illegal argument
-    public static void main(String[] args) {
-        Player player1 = new Player("Joe");
-        Game newGame = new Game(player1);
-        newGame.startRound();
-        System.out.println(newGame.getCurrentGameBoard());
-
-        boolean test = true;
-        while (test) {
-            try {
-                Scanner scanner = new Scanner(System.in);
-                String input;
-                char letter;
-                System.out.println(newGame.getCurrentGameBoard());
-                System.out.println("Pick a letter");
-                input = scanner.next();
-                letter = input.charAt(0);
-                newGame.pickLetter(letter);
-                test = false;
-            } catch (IllegalArgumentException e) {
-                System.out.println(e);
-            }
-        }
-    }
-
     @Test
-    public void testSpinWheel() {
+    public void testIfTheMathIsCorrectWhenBuyingConsonants() {
 
         System.out.println(newGame2.getCurrentPlayersTurn().getName()); // first player to go
         newGame2.setValueOfWheelSpin(500);
@@ -77,8 +50,10 @@ public class GameTest {
     }
 
     @Test
-    public void testGameToStringMethod() {
-        System.out.println(newGame2.toString());
+    public void testGameToStringMethodPrintsCorrectly() {
+        String expectedValue = "Player name: Joe\nTotal money: 0\nRound money: 0\n\n" +
+                "Player name: Cindy\nTotal money: 0\nRound money: 0\n\n";
+        assertTrue(expectedValue.equals(newGame2.toString()));
     }
 
     @Test
@@ -86,23 +61,75 @@ public class GameTest {
         String player1 = newGame2.getCurrentPlayersTurn().getName();
         newGame2.getCurrentPlayersTurn().setCurrentRoundMoney(1000);
         int spinWheel = 1000;
-        while(spinWheel > 0) {
+        while (spinWheel > 0) {
             spinWheel = newGame2.spinWheel();
         }
         System.out.println(spinWheel);
         String player2 = newGame2.getCurrentPlayersTurn().getName();
-        if(spinWheel == 0) {
+        if (spinWheel == 0) {
             newGame2.getNextPlayer();
             assertEquals(player1, newGame2.getCurrentPlayersTurn().getName());
-            assertEquals(1000,newGame2.getCurrentPlayersTurn().getCurrentRoundMoney());
-        } else if(spinWheel == -1) {
+            assertEquals(1000, newGame2.getCurrentPlayersTurn().getCurrentRoundMoney());
+        } else if (spinWheel == -1) {
             newGame2.getNextPlayer();
             assertEquals(player1, newGame2.getCurrentPlayersTurn().getName());
-            assertEquals(0,newGame2.getCurrentPlayersTurn().getCurrentRoundMoney());
+            assertEquals(0, newGame2.getCurrentPlayersTurn().getCurrentRoundMoney());
         }
     }
+
+    @Test
+    public void testIfWinningPlayerMethodSetsCorrectlyAndDisplaysTheCorrectWinner() {
+        newGame.getPlayers().get(0).setTotalMoney(10000);
+        newGame.setWinningPlayer();
+        String expectedValue = "Player name: Joe\nTotal money: 10000\nRound money: 0\n";
+        assertTrue(expectedValue.equals(newGame.getWinningPlayer()));
+    }
+
+    @Test
+    public void _testToEnsurePickConsonantThrowsAnIllegalArgumentExceptionAndThrowsTheRightMessage() {
+        try {
+            newGame.startRound();
+            newGame.pickLetter('1');
+        }
+        catch (IllegalArgumentException e) {
+            assertTrue(e.getMessage().equals("You must pick a valid consonant"));
+        }
+    }
+
+    @Test
+    public void _testToEnsureBuyAVowelThrowsAnIllegalArgumentExceptionAndThrowsTheRightMessage() {
+        try {
+            newGame.startRound();
+            newGame.getCurrentPlayersTurn().setCurrentRoundMoney(1000);
+            newGame.buyAVowel('1');
+        }
+        catch (IllegalArgumentException e) {
+            assertTrue(e.getMessage().equals("You must pick a valid vowel"));
+        }
+    }
+
+    @Test
+    public void testThatThePlayersRoundMoneyIsResetWhenAPlayerCorrectlySolvesThePuzzleAndThatPlayersTotalMoneyIsIncreasedToTheCorrectAmount() {
+        String solve = "HELLO";
+        newGame2.setCurrentPlayersTurn(newGame2.getPlayers().get(0));
+        newGame2.getCurrentPlayersTurn().setCurrentRoundMoney(1000);
+        newGame2.getCurrentPlayersTurn().setTotalMoney(1000);
+        assertEquals(1,newGame2.solvePuzzle(solve));
+        assertEquals(2000, newGame2.getPlayers().get(0).getTotalMoney());
+        assertEquals(0, newGame2.getPlayers().get(0).getCurrentRoundMoney());
+    }
+
+    @Test
+    public void testThatTheCurrentPlayerLosesTheirTurnIfTheySolvePuzzleIncorrectlyButRetainsTheirRoundEarnings() {
+        String solve = "GOODBYE";
+        newGame2.setCurrentPlayersTurn(newGame2.getPlayers().get(0));
+        newGame2.getCurrentPlayersTurn().setCurrentRoundMoney(1000);
+        assertEquals(0,newGame2.solvePuzzle(solve));
+        assertEquals(1000, newGame2.getPlayers().get(0).getCurrentRoundMoney());
+    }
+
+    @Test
+    public void testGetWinningPlayerReturnsTheStringThereIsNoWinner() {
+        
+    }
 }
-
-
-
-//jar cvfe jarexample.jar com.game.wheeloffortune Client.class
